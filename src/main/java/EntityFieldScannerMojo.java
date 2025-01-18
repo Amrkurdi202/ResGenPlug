c63b616db8f071ec44888c062d2cf429fa7afed0
amr <akurdi202@gmail.com>
2025-01-18 18:41:50 +0200

    bug fixes

--- src/main/java/EntityFieldScannerMojo.java
+++ src/main/java/EntityFieldScannerMojo.java
@@ -11,142 +11,175 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.*;
 
 @Mojo(name = "ResGenPlug", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
 public class EntityFieldScannerMojo extends AbstractMojo {
 
-  @Parameter(defaultValue = "${project.basedir}/src/main/java")
-  private File sourceDirectory;
+    @Parameter(defaultValue = "${project.basedir}/src/main/java")
+    private File sourceDirectory;
 
-  @Parameter(defaultValue = "${project.basedir}/src/main/resources/strings")
-  private File resourcesDirectory;
+    @Parameter(defaultValue = "${project.basedir}/src/main/resources/strings")
+    private File resourcesDirectory;
 
-  private static final String ENTITY_SUPERCLASS = "Entity";
-  private static final String RES_SUPERCLASS = "Res";
-  private Map<String, Set<String>> classHierarchy;
-  private File[] propertyFiles;
-  private Map<String, Properties> properties;
+    private static final String ENTITY_SUPERCLASS = "Entity";
+    private static final String RES_SUPERCLASS = "Res";
 
-  @Override
-  public void execute() throws MojoExecutionException {
-    try {
-      properties = new HashMap<>();
-      propertyFiles = resourcesDirectory.listFiles((dir, name) -> name.endsWith(".properties"));
-      if (propertyFiles == null) {
-        getLog().info("No resources found");
-        return;
-      }
-      for (File propertyFile : propertyFiles) {
-        Properties properties = new Properties();
-        properties.load(propertyFile.toURI().toURL().openStream());
-        this.properties.put(propertyFile.getName(), properties);
-      }
+    private static final String MULTI_ENTITIES_REFERENCES = "InternalMultiEntitiesReferences";
 
-      SourceRoot sourceRoot = new SourceRoot(Paths.get(sourceDirectory.toURI()));
-      classHierarchy = new HashMap<>();
+    private static final String CUSTOM_FIELD = "CustomField";
 
-      List<ParseResult<CompilationUnit>> compilationUnits = sourceRoot.tryToParse();
+    private static final String[] POTENTIAL_ANCESTORS = {RES_SUPERCLASS,
+            ENTITY_SUPERCLASS,
+            MULTI_ENTITIES_REFERENCES,
+            CUSTOM_FIELD};
 
-      for (ParseResult<CompilationUnit> parseResult : compilationUnits) {
-        Optional<CompilationUnit> result = parseResult.getResult();
-        result.ifPresent(compilationUnit -> {
-          compilationUnit.accept(new ClassVisitor(), classHierarchy);
-        });
-      }
-      for (ParseResult<CompilationUnit> parseResult : compilationUnits) {
-        Optional<CompilationUnit> result = parseResult.getResult();
-        result.ifPresent(compilationUnit -> {
-          compilationUnit.findAll(ClassOrInterfaceDeclaration.class).forEach(this::processClass);
-        });
-      }
+    private Map<String, Set<String>> classHierarchy;
+    private File[] propertyFiles;
+    private Map<String, Properties> properties;
 
-    } catch (IOException e) {
-      throw new MojoExecutionException("Error parsing source files", e);
-    }
-    for (File propertyFile : propertyFiles) {
-      try (FileWriter writer = new FileWriter(propertyFile, false)) {
-        properties.get(propertyFile.getName()).store(writer, null);
-      } catch (IOException e) {
-        getLog().error(e);
-        throw new RuntimeException(e);
-      }
-    }
-  }
-
-  private void processClass(ClassOrInterfaceDeclaration classDeclaration) {
-    getLog().info("Found class: " + classDeclaration.getNameAsString());
-    if (isAncestor(classDeclaration.getNameAsString(), ENTITY_SUPERCLASS, classHierarchy)) {
-      getLog().info("Found entity: " + classDeclaration.getFullyQualifiedName().orElse(""));
-      String className = classDeclaration.getFullyQualifiedName().orElse("");
-      Set<String> fieldNames = new HashSet<>();
-
-      classDeclaration.findAll(FieldDeclaration.class).forEach(field -> {
-        for (VariableDeclarator variable : field.getVariables()) {
-          if (isAncestor(variable.getTypeAsString(), RES_SUPERCLASS, classHierarchy)
-          ||isAncestor(variable.getTypeAsString(), ENTITY_SUPERCLASS, classHierarchy)) {
-            fieldNames.add(variable.getNameAsString());
-          }
-        }
-      });
-
-      updatePropertiesFile(className, fieldNames);
-    }
-  }
-
-  private static class ClassVisitor extends VoidVisitorAdapter<Map<String, Set<String>>> {
     @Override
-    public void visit(ClassOrInterfaceDeclaration cid, Map<String, Set<String>> classHierarchy) {
-      super.visit(cid, classHierarchy);
-      String className = cid.getNameAsString();
-      Set<String> supers = classHierarchy.computeIfAbsent(className, k -> new HashSet<>());
-      cid.getExtendedTypes().forEach(extendedType -> {
-        String superClassName = extendedType.getNameAsString();
-        supers.add(superClassName);
-      });
-      cid.getImplementedTypes().forEach(implementedType -> {
-        String interfaceName = implementedType.getNameAsString();
-        supers.add(interfaceName);
-      });
-    }
-  }
+    public void execute() throws MojoExecutionException {
+        classHierarchy = new HashMap<>();
+        for (int i = 0; i <2 ; i++) {
+            try {
+                properties = new HashMap<>();
+                propertyFiles = resourcesDirectory.listFiles((dir, name) -> name.endsWith(".properties"));
+                if (propertyFiles == null) {
+                    getLog().info("No resources found");
+                    return;
+                }
+                for (File propertyFile : propertyFiles) {
+                    Properties properties = new Properties();
+                    InputStream inputStream = propertyFile.toURI().toURL().openStream();
+                    try (InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
+                        properties.load(reader);
+                    }
+                    this.properties.put(propertyFile.getName(), properties);
+                }
 
-  private boolean isAncestor(String className, String potentialAncestor, Map<String, Set<String>> classHierarchy) {
-    if (classHierarchy.containsKey(className)) {
-      Set<String> superClasses = classHierarchy.get(className);
-      for (String superClass : superClasses) {
-        if (superClass.equals(potentialAncestor)) {
-          return true;
+                SourceRoot sourceRoot = new SourceRoot(Paths.get(sourceDirectory.toURI()));
+
+
+                List<ParseResult<CompilationUnit>> compilationUnits = sourceRoot.tryToParse();
+
+                for (ParseResult<CompilationUnit> parseResult : compilationUnits) {
+                    Optional<CompilationUnit> result = parseResult.getResult();
+                    result.ifPresent(compilationUnit -> {
+                        compilationUnit.accept(new ClassVisitor(), classHierarchy);
+                    });
+                }
+                for (ParseResult<CompilationUnit> parseResult : compilationUnits) {
+                    Optional<CompilationUnit> result = parseResult.getResult();
+                    result.ifPresent(compilationUnit -> {
+                        compilationUnit.findAll(ClassOrInterfaceDeclaration.class).forEach(this::processClass);
+                    });
+                }
+
+            } catch (IOException e) {
+                throw new MojoExecutionException("Error parsing source files", e);
+            }
+            for (File propertyFile : propertyFiles) {
+                try (FileWriter writer = new FileWriter(propertyFile, StandardCharsets.UTF_8, false)) {
+                    properties.get(propertyFile.getName()).store(writer, null);
+                } catch (IOException e) {
+                    getLog().error(e);
+                    throw new RuntimeException(e);
+                }
+            }
         }
-      }
-      for (String superClass : superClasses) {
-        return isAncestor(superClass, potentialAncestor, classHierarchy);
-      }
+        System.out.println(classHierarchy);
     }
-    return false;
-  }
 
-  private void updatePropertiesFile(String className, Set<String> fieldNames) {
+    private void processClass(ClassOrInterfaceDeclaration classDeclaration) {
+        getLog().info("Found class: " + classDeclaration.getNameAsString());
+        if (isAncestor(classDeclaration.getNameAsString(), ENTITY_SUPERCLASS, classHierarchy)) {
+            getLog().info("Found entity: " + classDeclaration.getFullyQualifiedName().orElse(""));
+            String className = classDeclaration.getFullyQualifiedName().orElse("");
+            Set<String> fieldNames = new HashSet<>();
 
-    for (File propertyFile : propertyFiles) {
-      Properties props = properties.get(propertyFile.getName());
-      for (String fieldName : fieldNames) {
-        String key = className + "." + fieldName;
-        if (!props.containsKey(key))
-          props.setProperty(key, "");
+            classDeclaration.findAll(FieldDeclaration.class).forEach(field -> {
+                for (VariableDeclarator variable : field.getVariables()) {
+                    String typeAsString = variable.getTypeAsString();
+                    int endIndex = typeAsString.lastIndexOf('<');
+                    if (endIndex != -1) {
+                        typeAsString = typeAsString.substring(0, endIndex);
+                        getLog().info("Found generic type: " + typeAsString);
+                    }
+                    typeAsString = typeAsString.replaceAll("\\s", "");
+                    if (isAncestorOfAny(typeAsString, classHierarchy)) {
+                        getLog().info("Found field: " + variable.getNameAsString());
+                        fieldNames.add(variable.getNameAsString());
+                    }
+                }
+            });
 
-        if (!props.containsKey(className))
-          props.setProperty(className, "");
-
-        String packageName = className.replaceAll(".\\w*$", "");
-        if (!props.containsKey(packageName))
-          props.setProperty(packageName, "");
-
-      }
+            updatePropertiesFile(className, fieldNames);
+        }
     }
-  }
+
+    private static class ClassVisitor extends VoidVisitorAdapter<Map<String, Set<String>>> {
+        @Override
+        public void visit(ClassOrInterfaceDeclaration cid, Map<String, Set<String>> classHierarchy) {
+            super.visit(cid, classHierarchy);
+            String className = cid.getNameAsString();
+            Set<String> supers = classHierarchy.computeIfAbsent(className, k -> new HashSet<>());
+            cid.getExtendedTypes().forEach(extendedType -> {
+                String superClassName = extendedType.getNameAsString();
+                supers.add(superClassName);
+            });
+            cid.getImplementedTypes().forEach(implementedType -> {
+                String interfaceName = implementedType.getNameAsString();
+                supers.add(interfaceName);
+            });
+        }
+    }
+
+    private boolean isAncestorOfAny(String className, Map<String, Set<String>> classHierarchy) {
+        for (String ancestor : EntityFieldScannerMojo.POTENTIAL_ANCESTORS) {
+            if (isAncestor(className, ancestor, classHierarchy)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isAncestor(String className, String potentialAncestor, Map<String, Set<String>> classHierarchy) {
+        if (classHierarchy.containsKey(className)) {
+            Set<String> superClasses = classHierarchy.get(className);
+            for (String superClass : superClasses) {
+                if (superClass.equals(potentialAncestor)) {
+                    return true;
+                }
+            }
+            for (String superClass : superClasses) {
+                return isAncestor(superClass, potentialAncestor, classHierarchy);
+            }
+        }
+        return false;
+    }
+
+    private void updatePropertiesFile(String className, Set<String> fieldNames) {
+
+        for (File propertyFile : propertyFiles) {
+            Properties props = properties.get(propertyFile.getName());
+
+            if (!props.containsKey(className))
+                props.setProperty(className, "");
+
+            String packageName = className.replaceAll(".\\w*$", "");
+            if (!props.containsKey(packageName))
+                props.setProperty(packageName, "");
+
+
+            for (String fieldName : fieldNames) {
+                String key = className + "." + fieldName;
+                if (!props.containsKey(key))
+                    props.setProperty(key, "");
+            }
+        }
+    }
 }
